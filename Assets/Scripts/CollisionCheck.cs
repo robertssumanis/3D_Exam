11b170694dcd2d1d35f9fa7c2c43c655b21a8c39
robertssumanis <sumanis4321@gmail.com>
2023-06-09 14:44:45 +0100

    exam

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+    private float points;
+    public float playedTime;
+
+    void Start()
+    {
+        playedTime = 0;
+    }
+
+    void Update(){
+    
+        playedTime += Time.deltaTime;
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player") 
+        {
+            Debug.Log("+1 point");
+            Destroy(gameObject);
+            points = points + 1;
+            print(points);
+            if (points == 4){
+                print("Your score in seconds: " + playedTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public int speed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, speed, 0);
+        }
+    }
+}
